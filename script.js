c803715fcadd4d99034abfdb9a2c0e3da40d5b31
shakil <shakilahamed0607@gmail.com>
2024-01-31 16:00:34 +0530

    second commit

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Software Developer", "Software Developer", "Software Developer"],
+        strings: [ "Software Developer", "Coder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings:   [ "Software Developer", "Software Developer", "Software Developer"],
+        strings:  [ "Software Developer", "Coder"] ,
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
